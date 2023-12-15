b3b344f15ec7efa45d303770712a93c5f8ebaf59
yuanadmin <yuanadmin@yuan.com>
2023-12-15 22:33:05 +0800

    fix docoment not defined for router of  /singer

--- pages/singer.vue
+++ pages/singer.vue
@@ -1,28 +1,22 @@
 <template>
-  <div
-    class="singer"
-    v-loading="!singers.length"
-  >
-    <index-list
-      :data="singers"
-      @select="selectSinger"
-    />
-    <router-view v-slot="{Component}">
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers" @select="selectSinger" />
+    <router-view v-slot="{ Component }">
       <transition appear name="slide">
-        <component :is="Component" :data="selectedSinger"/>
+        <component :is="Component" :data="selectedSinger" />
       </transition>
     </router-view>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, reactive, toRefs } from 'vue'
-import { useRouter } from 'vue-router'
-import { IndexList } from '@/components'
-import SingerServer from '@/api/singer'
-import { SINGER_KEY } from '@/utils/constants'
-import { saveSessionStorage } from '@/utils/cache'
-import type { Singer, Singers } from '@/types/api/singer'
+import { defineComponent, onMounted, reactive, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import { IndexList } from "@/components";
+import SingerServer from "@/api/singer";
+import { SINGER_KEY } from "@/utils/constants";
+import { saveSessionStorage } from "@/utils/cache";
+import type { Singer, Singers } from "@/types/api/singer";
 
 interface State {
   /** 歌手列表 */
@@ -32,48 +26,50 @@ interface State {
 }
 
 export default defineComponent({
-  name: 'Singer',
+  name: "Singer",
   components: {
-    IndexList
+    IndexList,
   },
-  setup () {
-    const router = useRouter()
-    const state = reactive<State>({
-      singers: [],
-      selectedSinger: undefined
-    })
+  setup() {
+    if (process.client) {
+      const router = useRouter();
+      const state = reactive<State>({
+        singers: [],
+        selectedSinger: undefined,
+      });
 
-    /** 获取数据 */
-    async function fetchData (): Promise<void> {
-      const { singers } = await SingerServer.getSingerList()
-      state.singers = singers
+      /** 获取数据 */
+      async function fetchData(): Promise<void> {
+        const { singers } = await SingerServer.getSingerList();
+        state.singers = singers;
+      }
+
+      /** 选择歌手 */
+      function selectSinger(singer: Singer): void {
+        state.selectedSinger = singer;
+        cacheSinger(singer);
+        router.push({
+          path: `/singer/${singer.mid}`,
+        });
+      }
+
+      /** 缓存歌手 */
+      function cacheSinger(singer: Singer): void {
+        saveSessionStorage(SINGER_KEY, singer);
+      }
+
+      onMounted(() => {
+        fetchData();
+      });
+
+      return {
+        ...toRefs(state),
+
+        selectSinger,
+      };
     }
-
-    /** 选择歌手 */
-    function selectSinger (singer: Singer): void {
-      state.selectedSinger = singer
-      cacheSinger(singer)
-      router.push({
-        path: `/singer/${singer.mid}`
-      })
-    }
-
-    /** 缓存歌手 */
-    function cacheSinger (singer: Singer): void {
-      saveSessionStorage(SINGER_KEY, singer)
-    }
-
-    onMounted(() => {
-      fetchData()
-    })
-
-    return {
-      ...toRefs(state),
-
-      selectSinger
-    }
-  }
-})
+  },
+});
 </script>
 
 <style scoped lang="less">
